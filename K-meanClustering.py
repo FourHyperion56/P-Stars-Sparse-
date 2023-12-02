f1afb3bc0bc545b5c5cd11cf5c45cf75d0988a29
toM0dots <144872686+toM0dots@users.noreply.github.com>
2023-12-02 15:50:23 -0800

    Update K-meanClustering.py

--- K-meanClustering.py
+++ K-meanClustering.py
@@ -57,3 +57,61 @@ plt.xlabel('mad')
 plt.ylabel('chi2')
 plt.legend()
 plt.show()
+
+
+######## new method
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Function to apply the elbow method
+def find_optimal_clusters(data):
+    wcss = []
+    for i in range(1, 11):  # Testing 1 to 10 clusters
+        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans.fit(data)
+        wcss.append(kmeans.inertia_)
+
+    # Determine the elbow point
+    k = wcss.index(min(wcss, key=lambda x: abs(x - np.mean(wcss)))) + 1
+    return k
+
+
+# Load the entire CSV file
+data = pd.read_csv('path_to_your_file.csv')
+
+# Assuming there's a column 'dataset_id' that identifies each dataset
+dataset_ids = data['dataset_id'].unique()
+
+for dataset_id in dataset_ids:
+    # Filter data for the current dataset
+    current_data = data[data['dataset_id'] == dataset_id]
+
+    # Extract the relevant columns
+    X = current_data[['x', 'y']].values
+
+    # Find the optimal number of clusters
+    optimal_clusters = find_optimal_clusters(X)
+
+    # Apply KMeans clustering
+    kmeans = KMeans(n_clusters=optimal_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X)
+    centroids = kmeans.cluster_centers_
+    labels = kmeans.labels_
+
+    # Plotting
+    plt.figure(figsize=(10, 5))
+    colors = ['r', 'g', 'b', 'y', 'c', 'm', 'orange', 'brown', 'pink', 'gray']
+    for i in range(optimal_clusters):
+        plt.scatter(X[labels == i, 0], X[labels == i, 1], s=100, c=colors[i], label=f'Cluster {i + 1}')
+
+    plt.scatter(centroids[:, 0], centroids[:, 1], s=300, c='black', marker='x', label='Centroids')
+    plt.title(f'Clusters for Dataset ID {dataset_id}')
+    plt.xlabel('x-coordinate')
+    plt.ylabel('y-coordinate')
+    plt.legend()
+    plt.show()
+
+
