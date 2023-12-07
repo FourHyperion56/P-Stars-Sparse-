2f2e59db2b5c81785a050175e8a71d52cd61f1ae
toM0dots <144872686+toM0dots@users.noreply.github.com>
2023-12-07 09:30:14 -0800

    Update K-meanClustering.py

--- K-meanClustering.py
+++ K-meanClustering.py
@@ -58,63 +58,72 @@ def clusterPlotter():
     plt.ylabel('chi2')
     plt.legend()
     plt.show()
+# new method
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# Function to apply the elbow method
+def find_optimal_clusters(data):
+    wcss = []
+    for i in range(1, 40):  # Testing 1 to 4 clusters
+        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans.fit(data)
+        wcss.append(kmeans.inertia_)
+
+    # Determine the elbow point
+    k = wcss.index(min(wcss, key=lambda x: abs(x - np.mean(wcss)))) + 1
+    return k
+
+# Load the entire CSV file
+# Update the path as per your file system
+data = pd.read_csv('/Users/tomarr626/Desktop/P Stars/Sparse/HCV.csv')
+
+# Assuming there's a column 'groupid' that identifies each dataset
+dataset_ids = data['groupid'].unique()
+
+for dataset_id in dataset_ids:
+    # Filter data for the current dataset
+    current_data = data[data['groupid'] == dataset_id]
+
+    # Extract the relevant columns
+    X = current_data[['chi2', 'mad']].values
+
+    # Standardize the features
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    # Find the optimal number of clusters
+    optimal_clusters = find_optimal_clusters(X_scaled)
+
+    # Apply KMeans clustering to the standardized data
+    kmeans = KMeans(n_clusters=optimal_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X_scaled)
+
+    # Get the cluster labels and centroids
+    labels = kmeans.labels_
+    centroids = kmeans.cluster_centers_
+
+    # Inverse transform the centroids for plotting in original scale
+    centroids_original_scale = scaler.inverse_transform(centroids)
+
+    # Plotting
+    plt.figure(figsize=(10, 5))
+    colors = ['r', 'g', 'b', 'y', 'c', 'm', 'orange', 'brown', 'pink', 'gray']
+    for i in range(optimal_clusters):
+        # Since we've scaled the data, we need to inverse transform the data points for plotting
+        cluster_data = scaler.inverse_transform(X_scaled[labels == i, :])
+        plt.scatter(cluster_data[:, 0], cluster_data[:, 1], s=100, c=colors[i], label=f'Cluster {i + 1}')
+        # Plot centroids in original scale
+        plt.scatter(centroids_original_scale[i, 0], centroids_original_scale[i, 1], s=300, c='black', marker='x')
+
+    plt.title(f'Clusters for Dataset ID {dataset_id}')
+    plt.xlabel('chi2')
+    plt.ylabel('mad')
+    plt.legend()
+    plt.show()
 
 
-def newClusterPlotter(): 
-    ######## new method
-    import pandas as pd
-    from sklearn.cluster import KMeans
-    import matplotlib.pyplot as plt
-    import numpy as np
 
-
-    # Function to apply the elbow method
-    def find_optimal_clusters(data):
-        wcss = []
-        for i in range(1, 11):  # Testing 1 to 10 clusters
-            kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-            kmeans.fit(data)
-            wcss.append(kmeans.inertia_)
-
-        # Determine the elbow point
-        k = wcss.index(min(wcss, key=lambda x: abs(x - np.mean(wcss)))) + 1
-        return k
-
-
-    # Load the entire CSV file
-    groupId = 1040910
-    data = pd.read_csv(file_path)
-
-    # Assuming there's a column 'dataset_id' that identifies each dataset
-    dataset_ids = data["groupid"].unique()
-
-    for dataset_id in dataset_ids:
-        # Filter data for the current dataset
-        current_data = data[data["groupid"] == dataset_id]
-
-        # Extract the relevant columns
-        X = current_data[['chi2', 'mad']].values
-
-        # Find the optimal number of clusters
-        optimal_clusters = find_optimal_clusters(X)
-
-        # Apply KMeans clustering
-        kmeans = KMeans(n_clusters=optimal_clusters, init='k-means++', max_iter=300, n_init=10, random_state=0)
-        kmeans.fit(X)
-        centroids = kmeans.cluster_centers_
-        labels = kmeans.labels_
-
-        # Plotting
-        plt.figure(figsize=(10, 5))
-        colors = ['r', 'g', 'b', 'y', 'c', 'm', 'orange', 'brown', 'pink', 'gray']
-        for i in range(optimal_clusters):
-            plt.scatter(X[labels == i, 0], X[labels == i, 1], s=100, c=colors[i], label=f'Cluster {i + 1}')
-
-        plt.scatter(centroids[:, 0], centroids[:, 1], s=300, c='black', marker='x', label='Centroids')
-        plt.title(f'Clusters for Dataset ID {dataset_id}')
-        plt.xlabel('chi2')
-        plt.ylabel('mad')
-        plt.legend()
-        plt.show()
-
-newClusterPlotter()
