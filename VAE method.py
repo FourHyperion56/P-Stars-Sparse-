8cad045c9d10432a7c9c76fde4824c22df2b1d8c
toM0dots <144872686+toM0dots@users.noreply.github.com>
2023-12-07 10:15:39 -0800

    Update VAE method.py

--- VAE method.py
+++ VAE method.py
@@ -1,62 +1,66 @@
-#!pip install tensorflow
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Lambda
-from tensorflow.keras.models import Model
-from tensorflow.keras.losses import binary_crossentropy
-from tensorflow.keras import backend as K
+from tensorflow.keras import layers, Model
+from tensorflow.keras.datasets import mnist
+import numpy as np
 
-# Dataset dimensions
-original_dim = # number of features in your dataset
-input_shape = (original_dim,)
-intermediate_dim = 64
-latent_dim = 2  # Dimension of the latent space
+class Sampling(layers.Layer):
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
-# VAE model
-inputs = Input(shape=input_shape, name='encoder_input')
-x = Dense(intermediate_dim, activation='relu')(inputs)
-z_mean = Dense(latent_dim, name='z_mean')(x)
-z_log_var = Dense(latent_dim, name='z_log_var')(x)
+# Build the encoder
+latent_dim = 2
 
-# Use reparameterization trick to ensure correct gradient
-def sampling(args):
-    z_mean, z_log_var = args
-    batch = K.shape(z_mean)[0]
-    dim = K.int_shape(z_mean)[1]
-    epsilon = K.random_normal(shape=(batch, dim))
-    return z_mean + K.exp(0.5 * z_log_var) * epsilon
+encoder_inputs = layers.Input(shape=(28, 28, 1))
+x = layers.Flatten()(encoder_inputs)
+x = layers.Dense(64, activation='relu')(x)
+z_mean = layers.Dense(latent_dim, name='z_mean')(x)
+z_log_var = layers.Dense(latent_dim, name='z_log_var')(x)
+z = Sampling()([z_mean, z_log_var])
+encoder = Model(encoder_inputs, [z_mean, z_log_var, z], name='encoder')
 
-z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+# Build the decoder
+latent_inputs = layers.Input(shape=(latent_dim,))
+x = layers.Dense(64, activation='relu')(latent_inputs)
+x = layers.Dense(784, activation='sigmoid')(x)
+decoder_outputs = layers.Reshape((28, 28, 1))(x)
+decoder = Model(latent_inputs, decoder_outputs, name='decoder')
 
-# Instantiate encoder
-encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+# Define the VAE as a `Model` with a custom `train_step`
+class VAE(Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
 
-# Build decoder
-latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-x = Dense(intermediate_dim, activation='relu')(latent_inputs)
-outputs = Dense(original_dim, activation='sigmoid')(x)
-
-# Instantiate decoder
-decoder = Model(latent_inputs, outputs, name='decoder')
-
-# Instantiate VAE
-outputs = decoder(encoder(inputs)[2])
-vae = Model(inputs, outputs, name='vae_mlp')
-
-# VAE loss
-reconstruction_loss = binary_crossentropy(inputs, outputs)
-reconstruction_loss *= original_dim
-kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
-kl_loss = K.sum(kl_loss, axis=-1)
-kl_loss *= -0.5
-vae_loss = K.mean(reconstruction_loss + kl_loss)
-vae.add_loss(vae_loss)
-vae.compile(optimizer='adam')
-
-# Load your dataset
-# Replace this with loading and preprocessing your dataset
-x_train = np.random.rand(100, original_dim)  # Example data
-x_test = np.random.rand(20, original_dim)    # Example data
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                tf.keras.losses.binary_crossentropy(data, reconstruction)
+            )
+            reconstruction_loss *= 28 * 28
+            kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+            kl_loss = tf.reduce_mean(kl_loss)
+            kl_loss *= -0.5
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        return {
+            "loss": total_loss,
+            "reconstruction_loss": reconstruction_loss,
+            "kl_loss": kl_loss,
+        }
 
 # Train the VAE
-vae.fit(x_train, epochs=50, batch_size=32, validation_data=(x_test, None))
+(x_train, _), (x_test, _) = mnist.load_data()
+mnist_digits = np.expand_dims(x_train, -1).astype("float32") / 255
+
+vae = VAE(encoder, decoder)
+vae.compile(optimizer=tf.keras.optimizers.Adam())
+vae.fit(mnist_digits, epochs=30, batch_size=128)
